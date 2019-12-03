8b8b9930e219f941f8a07786aa31f2f76a6e790f
Travenous <58489985+Travenous@users.noreply.github.com>
2019-12-03 17:37:59 -0500

    Update questionScripts.js
    Added color for question text and IDs for yes and no buttons

--- questionScripts.js
+++ questionScripts.js
@@ -3,8 +3,9 @@
 function movieQuestion(){
 
     document.body.style.backgroundColor = "lightBlue";
-    document.getElementById("questionToAsk").innerHTML = "Does this clip intrests you?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion1()'>Yes</button><button onclick = 'movieQuestion2()'>No</button>";
+  document.getElementById("questionToAsk").style.color = "red";
+    document.getElementById("questionToAsk").innerHTML = "Does this clip interest you?";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion1()'>Yes</button><button id='noButton' onclick = 'movieQuestion2()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/Eh993__rOxA";
 
 }
@@ -12,8 +13,9 @@ function movieQuestion(){
 function movieQuestion1(){
 
     document.body.style.backgroundColor = "Crimson";
+	document.getElementById("questionToAsk").style.color = "white";
     document.getElementById("questionToAsk").innerHTML = "It seems you may like romance. Do you like this clip?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion3()'>Yes</button><button onclick = 'movieQuestion4()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion3()'>Yes</button><button id='noButton' onclick = 'movieQuestion4()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/s7a_9CggQuE";
 
 }
@@ -22,8 +24,9 @@ function movieQuestion1(){
 function movieQuestion2(){
 
     document.body.style.backgroundColor = "Brown ";
+	document.getElementById("questionToAsk").style.color = "white";
     document.getElementById("questionToAsk").innerHTML = "I see. You probably do not like romance movies much. How about this one?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion16()'>Yes</button><button onclick = 'movieQuestion6()'>No</button>";
+   document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion5()'>Yes</button><button id='noButton' onclick = 'movieQuestion6()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/Egcx63-FfTE";
 
 }
@@ -32,8 +35,9 @@ function movieQuestion2(){
 function movieQuestion3(){
 
     document.body.style.backgroundColor = "DeepPink";
+	document.getElementById("questionToAsk").style.color = "#f5e0df";
     document.getElementById("questionToAsk").innerHTML = "Hmmm. I think I almost got it. What about this clip?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer0()'>Yes</button><button onclick = 'answer1()'>No</button>";
+   document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer0()'>Yes</button><button id='noButton' onclick = 'answer1()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/Z2lYA7L7PZY";
 
 }
@@ -41,32 +45,20 @@ function movieQuestion3(){
 function movieQuestion4(){
 
     document.body.style.backgroundColor = "HotPink";
-    document.getElementById("questionToAsk").innerHTML = "I promise i'm not a mind reader. How about this one?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer1()'>Yes</button><button onclick = 'answer0()'>No</button>";
+	document.getElementById("questionToAsk").style.color = "blue";
+    document.getElementById("questionToAsk").innerHTML = "I see. You probably do not like romance movies much. How about this one?";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer1()'>Yes</button><button id='noButton' onclick = 'answer0()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/0pdqf4P9MB8";
 
 }
-
-
-//see mystery thriller
-function movieQuestion16(){
-
-    document.body.style.backgroundColor = "HotPink";
-    document.getElementById("questionToAsk").innerHTML = "Do you like this one too?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion5()'>Yes</button><button onclick = 'answer3()'>No</button>";
-    document.getElementById("video").src = "https://www.youtube.com/embed/lfqHb6INj3w";
-
-
-
-}
-
 
 //see if they like crime drama. if they don't give a list of mystery action otherwise a list of crime drama.
 function movieQuestion5(){
 
     document.body.style.backgroundColor = "GoldenRod";
-    document.getElementById("questionToAsk").innerHTML = "Let's narrow this genre one more time. Do you like this trailer?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer2()'>Yes</button><button onclick = 'answer14()'>No</button>";
+	document.getElementById("questionToAsk").style.color = "black";
+    document.getElementById("questionToAsk").innerHTML = "Let's narrow some genres in this category then. Do you like this trailer?";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer2()'>Yes</button><button id='noButton' onclick = 'answer3()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/BV_nssS6Zkg";
 
 }
@@ -75,8 +67,9 @@ function movieQuestion5(){
 function movieQuestion6(){
 
     document.body.style.backgroundColor = "Cyan";
+	document.getElementById("questionToAsk").style.color = "red";
     document.getElementById("questionToAsk").innerHTML = "Maybe that wasn't your cup of tea. How about this clip?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion7()'>Yes</button><button onclick = 'movieQuestion8()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer3()'>Yes</button><button id='noButton' onclick = 'answer2()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/kl8F-8tR8to";
 
 }
@@ -85,8 +78,9 @@ function movieQuestion6(){
 function movieQuestion7(){
 
     document.body.style.backgroundColor = "#19ffff";
+	document.getElementById("questionToAsk").style.color = "#f5e0df";
     document.getElementById("questionToAsk").innerHTML = "Hmmm ok ok. I see you like action stuff. Does this excite you?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer4()'>Yes</button><button onclick = 'movieQuestion9()'>No</button>";
+   document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer4()'>Yes</button><button id='noButton' onclick = 'movieQuestion9()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/7GqClqvlObY";
 
 }
@@ -95,8 +89,9 @@ function movieQuestion7(){
 function movieQuestion9(){
 
     document.body.style.backgroundColor = "##e6ffff";
+	document.getElementById("questionToAsk").style.color = "red";
     document.getElementById("questionToAsk").innerHTML = "Do you want to be a superhero, too?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer5()'>Yes</button><button onclick = 'answer6()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer5()'>Yes</button><button id='noButton' onclick = 'answer6()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/8wNgphPi5VM";
 
 }
@@ -106,8 +101,9 @@ function movieQuestion9(){
 function movieQuestion8(){
 
     document.body.style.backgroundColor = "DarkViolet";
+	document.getElementById("questionToAsk").style.color = "blue";
     document.getElementById("questionToAsk").innerHTML = "Do you like space stuff?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion10()'>Yes</button><button onclick = 'movieQuestion11()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion10()'>Yes</button><button id='noButton' onclick = 'movieQuestion11()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/2LqzF5WauAw";
 
 }
@@ -117,8 +113,9 @@ function movieQuestion8(){
 function movieQuestion10(){
 
     document.body.style.backgroundColor = "DarkViolet";
+	document.getElementById("questionToAsk").style.color = "blue";
     document.getElementById("questionToAsk").innerHTML = "Do you spooky AI apocalypses?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer7()'>Yes</button><button onclick = 'movieQuestion12()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer7()'>Yes</button><button id='noButton' onclick = 'movieQuestion12()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/EoQuVnKhxaM";
 
 }
@@ -127,8 +124,9 @@ function movieQuestion10(){
 function movieQuestion12(){
 
     document.body.style.backgroundColor = "DarkViolet";
+	document.getElementById("questionToAsk").style.color = "blue";
     document.getElementById("questionToAsk").innerHTML = "My bad, let's take a step back.?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer8()'>Yes</button><button onclick = 'answer9()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer8()'>Yes</button><button id='noButton' onclick = 'answer9()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/5wfrDhgUMGI";
 
 }
@@ -137,8 +135,9 @@ function movieQuestion12(){
 function movieQuestion11(){
 
     document.body.style.backgroundColor = "Tomato";
+	document.getElementById("questionToAsk").style.color = "white";
     document.getElementById("questionToAsk").innerHTML = "OK, let's try something spooky?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion13()'>Yes</button><button onclick = 'final()'>No</button>";
+   document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion13()'>Yes</button><button id='noButton' onclick = 'final()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/k10ETZ41q5o";
 
 }
@@ -147,8 +146,9 @@ function movieQuestion11(){
 function movieQuestion13(){
 
     document.body.style.backgroundColor = "Tomato";
+	document.getElementById("questionToAsk").style.color = "white";
     document.getElementById("questionToAsk").innerHTML = "What about this?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'movieQuestion14()'>Yes</button><button onclick = 'movieQuestion15()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'movieQuestion14()'>Yes</button><button id='noButton' onclick = 'movieQuestion15()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/8m9EVP8X7N8";
 
 }
@@ -157,8 +157,9 @@ function movieQuestion13(){
 function movieQuestion14(){
 
     document.body.style.backgroundColor = "Tomato";
+	document.getElementById("questionToAsk").style.color = "white";
     document.getElementById("questionToAsk").innerHTML = "Interesting. Do you like this then?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer10()'>Yes</button><button onclick = 'answer13()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer10()'>Yes</button><button id='noButton' onclick = 'answer13()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/HUjZHFNMlOM";
 
 }
@@ -168,8 +169,9 @@ function movieQuestion14(){
 function movieQuestion15(){
 
     document.body.style.backgroundColor = "Tomato";
+	document.getElementById("questionToAsk").style.color = "blue";
     document.getElementById("questionToAsk").innerHTML = "Do you like this?";
-    document.getElementById("btnArea").innerHTML = "<button onclick = 'answer11()'>Yes</button><button onclick = 'answer12()'>No</button>";
+    document.getElementById("btnArea").innerHTML = "<button id='yesButton' onclick = 'answer11()'>Yes</button><button id='noButton' onclick = 'answer12()'>No</button>";
     document.getElementById("video").src = "https://www.youtube.com/embed/zuZnRUcoWos";
 
 
@@ -277,3 +279,6 @@ function final(){
 
 
 }
+
+    
+
